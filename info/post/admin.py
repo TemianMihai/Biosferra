6f94f5c08c7e2c44449b34fe2bc99941b809d32d
temi4000 <mihai.temian@gmail.com>
2018-05-01 16:14:45 +0300

    Avem comms

--- info/post/admin.py
+++ info/post/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from models import PostModel
+from models import PostModel, Comment
 
 
 class PostModelAdmin(admin.ModelAdmin):
     list_display = ['name', 'details', 'delivery_time', 'cantity', 'price']
 
 admin.site.register(PostModel, PostModelAdmin)
+admin.site.register(Comment)
