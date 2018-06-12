b7c30634869cedc7d8d56cc46ebd4d0942504d38
temi4000 <mihai.temian@gmail.com>
2018-06-12 23:27:14 +0300

    favorites

--- info/user_profile/admin.py
+++ info/user_profile/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
-from .models import Mesaje, Report
+from .models import Mesaje, Report, Favorit
 
 class MesajeAdmin(admin.ModelAdmin):
     list_display = ['titlu', 'autor', 'destinatar']
 
 class ReportAdmin(admin.ModelAdmin):
-    list_display = ['titlu', 'autor', 'destinatar']
+    list_display = ['titluu', 'autor', 'destinatar']
+
+class FavoritAdmin(admin.ModelAdmin):
+    list_display = ['alegator', 'ales']
 
 admin.site.register(Mesaje, MesajeAdmin)
 admin.site.register(Report, ReportAdmin)
+admin.site.register(Favorit, FavoritAdmin)
--- info/user_profile/views.py
+++ info/user_profile/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from .forms import Edit_profile, Edit_profile2, CreateMesajeForm, CreateReportForm
+from django.conf import settings
+from django.shortcuts import render, redirect
+from .forms import Edit_profile, Edit_profile2, CreateMesajeForm, CreateReportForm, CreateFavoritForm
 from authentication.models import Account2
 from post.models import PostModel
-from django.contrib.auth.models import User
+from .models import Favorit
+from django.core.mail import send_mail
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseForbidden
 
 # Create your views here.
 @login_required
@@ -26,14 +27,14 @@ def profile_detail(request):
     })
 
 
-
-
 def profile(request, slug):
     current_user = request.user
     anunturi = PostModel.objects.all()
+    favoriit = Favorit.objects.all()
     user2 = Account2.objects.get(slug=slug)
     form = CreateMesajeForm(request.POST or None)
     form2 = CreateReportForm(request.POST or None)
+    form3 = CreateFavoritForm(request.POST or None)
     if request.method == 'POST':
         if form.is_valid():
             mesaj = form.instance
@@ -45,6 +46,18 @@ def profile(request, slug):
             report.autor = current_user
             report.destinatar = user2.user
             form2.save()
+            subject = 'Report'
+            message = "Userul: %s a trimis un report catre: %s" %(form2.instance.autor, form2.instance.destinatar)
+            from_email = settings.EMAIL_HOST_USER
+            to_list = [settings.EMAIL_HOST_USER]
+            send_mail(subject, message, from_email, to_list, fail_silently=True)
+        if form3.is_valid():
+            favorit = form3.instance
+            favorit.alegator = current_user
+            favorit.ales = user2.user
+            if favorit.favorite == False:
+                favorit.favorite = True
+                form3.save()
     query = request.GET.get("q")
     if query:
         anunturi = anunturi.filter(name__contains=query)
@@ -53,5 +66,7 @@ def profile(request, slug):
         'anunturi':anunturi,
         'form':form,
         'form2':form2,
+        'form3':form3,
         'user2':user2
+
     })
