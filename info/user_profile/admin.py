761c6354061d8f646f6b585106b8e1bd69c95777
temi4000 <mihai.temian@gmail.com>
2018-06-14 18:07:34 +0300

    profile

--- info/user_profile/admin.py
+++ info/user_profile/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Mesaje, Report, Favorit
+from .models import Mesaje, Report, Favorit, Profile
 
 class MesajeAdmin(admin.ModelAdmin):
     list_display = ['titlu', 'autor', 'destinatar']
@@ -10,6 +10,10 @@ class ReportAdmin(admin.ModelAdmin):
 class FavoritAdmin(admin.ModelAdmin):
     list_display = ['alegator', 'ales']
 
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['userul', 'produs1', 'produs2', 'produs3', 'produs4', 'produs5', 'image1', 'image2', 'descriere']
+
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Mesaje, MesajeAdmin)
 admin.site.register(Report, ReportAdmin)
 admin.site.register(Favorit, FavoritAdmin)
