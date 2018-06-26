aa8f042767519246a45b39d8a503b8c6b017fc37
temi4000 <mihai.temian@gmail.com>
2018-06-26 15:29:11 +0300

    comenzi

--- info/post/urls.py
+++ info/post/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
 	url(r'^create-post/', create_post, name='post-create'),
     url(r'^post/(?P<slug>[-\w]+)/$', get_post, name='post-get'),
     url(r'^post/(?P<slug>[\w-]+)/delete/$', delete_post, name='delete-post'),
-    url(r'^cosul-meu/$', produsele, name='produse')
+    url(r'^cosul-meu/$', produsele, name='produse'),
+    url(r'^comenzi/$', comenzi, name='comenzi')
+
 ]
