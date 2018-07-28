66aa692b1d0d0b8f97736ced3670ef1f3f98baae
temi4000 <mihai.temian@gmail.com>
2018-07-28 19:32:50 +0300

    PostgreSQL #66

--- info/info/settings.py
+++ info/info/settings.py
@@ -100,8 +100,12 @@ WSGI_APPLICATION = 'info.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'biosferra',
+        'USER': 'biosferra',
+        'PASSWORD': 'biosferra#2018',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
